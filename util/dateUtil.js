aeb489f656f3f0a28325a838e74d8f777b17d387
Blake Eriks <eblake98@vt.edu>
2021-11-19 20:22:02 -1000

    Handy date utility

---
+++ util/dateUtil.js
@@ -0,0 +1,17 @@
+const getUTCDateFromEpoch = epoch => {
+    const date = new Date(0)
+    date.setUTCMilliseconds(epoch)
+    return date
+}
+
+const getTimePeriods = () => {
+    const today = new Date()
+    const yearAgo = new Date(today), monthAgo = new Date(today), weekAgo = new Date(today), dayAgo = new Date(today)
+    yearAgo.setFullYear(yearAgo.getFullYear() - 1)
+    monthAgo.setMonth(monthAgo.getMonth() - 1)
+    weekAgo.setDate(weekAgo.getDate() - 7)
+    dayAgo.setDate(dayAgo.getDate() - 1)
+    return {today, dayAgo, weekAgo, monthAgo, yearAgo}
+}
+
+module.exports = {getUTCDateFromEpoch, getTimePeriods}
