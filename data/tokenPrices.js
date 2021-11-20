d79ca598e7bfd5d16d4fc3be6add3a916e94c89b
Blake Eriks <eblake98@vt.edu>
2021-11-20 10:08:49 -1000

    More static data

---
+++ data/tokenPrices.js
@@ -0,0 +1,188 @@
+module.exports = {
+    "status": {
+      "elapsed": 76,
+      "timestamp": "2021-11-20T17:02:31.929960159Z"
+    },
+    "data": [
+      {
+        "slug": "bitcoin",
+        "symbol": "BTC",
+        "metrics": {
+          "market_data": {
+            "price_usd": 57898.45117424977
+          }
+        }
+      },
+      {
+        "slug": "ethereum",
+        "symbol": "ETH",
+        "metrics": {
+          "market_data": {
+            "price_usd": 4251.55118478815
+          }
+        }
+      },
+      {
+        "slug": "binance-coin",
+        "symbol": "BNB",
+        "metrics": {
+          "market_data": {
+            "price_usd": 582.0191371995983
+          }
+        }
+      },
+      {
+        "slug": "tether",
+        "symbol": "USDT",
+        "metrics": {
+          "market_data": {
+            "price_usd": 1.0041054151780273
+          }
+        }
+      },
+      {
+        "slug": "solana",
+        "symbol": "SOL",
+        "metrics": {
+          "market_data": {
+            "price_usd": 209.43786439047474
+          }
+        }
+      },
+      {
+        "slug": "cardano",
+        "symbol": "ADA",
+        "metrics": {
+          "market_data": {
+            "price_usd": 1.887590851925801
+          }
+        }
+      },
+      {
+        "slug": "xrp",
+        "symbol": "XRP",
+        "metrics": {
+          "market_data": {
+            "price_usd": 1.0749800308731199
+          }
+        }
+      },
+      {
+        "slug": "polkadot",
+        "symbol": "DOT",
+        "metrics": {
+          "market_data": {
+            "price_usd": 40.3004907806513
+          }
+        }
+      },
+      {
+        "slug": "usd-coin",
+        "symbol": "USDC",
+        "metrics": {
+          "market_data": {
+            "price_usd": 1.0010600053167864
+          }
+        }
+      },
+      {
+        "slug": "dogecoin",
+        "symbol": "DOGE",
+        "metrics": {
+          "market_data": {
+            "price_usd": 0.22775328058809974
+          }
+        }
+      },
+      {
+        "slug": "avalanche",
+        "symbol": "AVAX",
+        "metrics": {
+          "market_data": {
+            "price_usd": 124.52240052128417
+          }
+        }
+      },
+      {
+        "slug": "shiba-inu",
+        "symbol": "SHIB",
+        "metrics": {
+          "market_data": {
+            "price_usd": 0.00004665273344319949
+          }
+        }
+      },
+      {
+        "slug": "terra",
+        "symbol": "LUNA",
+        "metrics": {
+          "market_data": {
+            "price_usd": 41.959898091831576
+          }
+        }
+      },
+      {
+        "slug": "litecoin",
+        "symbol": "LTC",
+        "metrics": {
+          "market_data": {
+            "price_usd": 215.221469364628
+          }
+        }
+      },
+      {
+        "slug": "wrapped-bitcoin",
+        "symbol": "WBTC",
+        "metrics": {
+          "market_data": {
+            "price_usd": 57946.02854698714
+          }
+        }
+      },
+      {
+        "slug": "crypto-com-chain",
+        "symbol": "CRO",
+        "metrics": {
+          "market_data": {
+            "price_usd": 0.5485774017180269
+          }
+        }
+      },
+      {
+        "slug": "uniswap",
+        "symbol": "UNI",
+        "metrics": {
+          "market_data": {
+            "price_usd": 21.195775291005578
+          }
+        }
+      },
+      {
+        "slug": "binance-usd",
+        "symbol": "BUSD",
+        "metrics": {
+          "market_data": {
+            "price_usd": 0.9999375437328065
+          }
+        }
+      },
+      {
+        "slug": "genesis-vision",
+        "symbol": "GVT",
+        "metrics": {
+          "market_data": {
+            "price_usd": 2933.1675140773177
+          }
+        }
+      },
+      {
+        "slug": "chainlink",
+        "symbol": "LINK",
+        "metrics": {
+          "market_data": {
+            "price_usd": 27.592669639217046
+          }
+        }
+      }
+    ]
+  }
