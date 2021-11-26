225d30e3f0f9a73b82fc1574ead4556843bb2213
Blake Eriks <35611123+BlakeEriks@users.noreply.github.com>
2021-11-26 13:10:39 -1000

    Update server.js
    Fix merge

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
 const { PORT = 4000, SECRET } = process.env;
 const express = require("express")
-// import express
-const express = require("express");
-const app = express();
-// routers
-const UserRouter = require("./controllers/user");
-const TokenRouter = require("./controllers/token")
 const tokenService = require('./tokenService')
 const middleware = require("./util/middleware")
-const tokenService = require('./tokenService')
 const app = express()
 
 middleware(app)
