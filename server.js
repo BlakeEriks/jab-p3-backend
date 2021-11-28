111c51e69b3820c87f746b2d2956a17adad375b5
Blake Eriks <35611123+BlakeEriks@users.noreply.github.com>
2021-11-28 11:55:30 -1000

    Normalize

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { PORT = 4000, SECRET } = process.env;
+const { PORT = 4000, SECRET } = process.env
 const express = require("express")
 const tokenService = require('./tokenService')
 const middleware = require("./util/middleware")
