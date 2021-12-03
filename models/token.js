2058dceaf3e70d9550b128bc1552ee516d25ebd3
Blake Eriks <eblake98@vt.edu>
2021-12-03 13:46:56 -1000

    Remove id's from nested schemas

---
+++ models/token.js
@@ -0,0 +1,26 @@
+// mongoose object
+mongoose = require("./connection")
+
+const HistoryDataSchema = new mongoose.Schema({
+    timestamp: {type: Date, required: true},
+    value: {type: Number, required: true}
+}, {_id: false})
+
+const HistorySchema = new mongoose.Schema({
+    period: {type: String, required: true, unique: true},
+    interval: {type: Number, required: true, unique: true},
+    data: [HistoryDataSchema]
+}, {_id: false})
+
+const TokenSchema = new mongoose.Schema({
+    name: {type: String, required: true, unique: true},
+    symbol: {type: String, required: true, unique: true},
+    price: {type: Number, required: true},
+    percentChange: {type: Number, required: true},
+    history: [HistorySchema]
+});
+
+const Token = mongoose.model("Token", TokenSchema);
+
+// export
+module.exports = Token
