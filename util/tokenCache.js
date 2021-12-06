947e442c182a1b77f76fd8af5a061a292cb43049
Blake Eriks <eblake98@vt.edu>
2021-12-06 15:09:30 -0800

    Adding time based cache invalidation

--- util/tokenCache.js
+++ util/tokenCache.js
@@ -2,11 +2,17 @@ const Token = require("../models/token")
 
 var isStale = true
 var tokenCache = []
+var lastSync = new Date()
 
 const get = async () => {
+    if (new Date().getTime() - lastSync.getTime() > 600000) {
+        isStale = true
+    }
+
     if (isStale) {
         console.log('Fetching fresh cache')
         tokenCache = await Token.find({})
+        lastSync = new Date()
         isStale = false
     }
     return tokenCache
